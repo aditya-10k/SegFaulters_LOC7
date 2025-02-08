68e8fc306ee8ba990717edea78f6d477126d3b1b
afrozAnsaari <afrozansaari787@gmail.com>
2025-02-09 02:40:12 +0530

    Update and rename requestroutes.js to requestRoutes.js

--- routes/requestroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const requestController = require("../controllers/collabcontroller");
-
-router.post("/send", requestController.sendRequest);
-router.put("/accept", requestController.acceptRequest);
-router.put("/reject", requestController.rejectRequest);
-
-module.exports = router;
