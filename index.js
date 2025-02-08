36a3d290b71a5da3d6d3f35938c5f66ddcdc9915
afrozAnsaari <afrozansaari787@gmail.com>
2025-02-09 01:31:31 +0530

    Update index.js with listings modules

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectToDatabase= require('./config/database.js'); 
+const connectToDatabase = require('./config/database.js');
 const authRoutes = require('./routes/authroutes');
 const geoRoutes = require('./routes/geoRoutes')
 const dataRoutes = require('./routes/dataRoutes')
 const requestRoutes = require('./routes/requestroutes');
 const cookieParser = require('cookie-parser');
+
+//All Listings here
+const listings = require("./routes/listingRoutes");
+
 const server = express();
 server.use(express.json());
 server.use(cookieParser());
 server.use(cors());
-
-server.use('/api/auth',authRoutes) ;
+//Listings
+server.use('/api/listings', listings)
+server.use('/api/auth', authRoutes);
 server.use('/api/geoR', geoRoutes);
 server.use('/api/data', dataRoutes);
 server.use('/api/data', requestRoutes);
